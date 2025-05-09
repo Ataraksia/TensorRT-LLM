Ataraksia/TensorRT-LLM

commit cfeabdf437c2ebe8576ff1ba309ea5a8b96f38f3

chore: Clean up the legacy DeepseekAllreudceFusionOp. (#4081)
Signed-off-by: Yukun He <23156053+hyukn@users.noreply.github.com>

tensorrt_llm/_torch/distributed/__init__.py
@@ -1,7 +1,7 @@
 from .communicator import Distributed, MPIDist, PPComm, TorchDist
 from .ops import (AllReduce, AllReduceFusionOp, AllReduceParams,
-                  AllReduceStrategy, DeepseekAllReduce, MoEAllReduce, allgather,
-                  reducescatter, userbuffers_allreduce_finalize)
+                  AllReduceStrategy, MoEAllReduce, allgather, reducescatter,
+                  userbuffers_allreduce_finalize)
 
 __all__ = [
     "allgather",
@@ -11,7 +11,6 @@ __all__ = [
     "AllReduceParams",
     "AllReduceFusionOp",
     "AllReduceStrategy",
-    "DeepseekAllReduce",
     "MoEAllReduce",
     "TorchDist",
     "PPComm",

cpp/tensorrt_llm/thop/CMakeLists.txt
@@ -42,7 +42,6 @@ add_library(
   convertSpecDecodingMaskToPackedMaskOp.cpp
   cutlassScaledMM.cpp
   cublasScaledMM.cpp
-  deepseekAllreduceFusionOp.cpp
   dynamicDecodeOp.cpp
   fmhaPackMaskOp.cpp
   fp8Op.cpp

cpp/tensorrt_llm/thop/deepseekAllreduceFusionOp.cpp
@@ -1,271 +1,0 @@
-/*
- * SPDX-FileCopyrightText: Copyright (c) 1993-2024 NVIDIA CORPORATION &
- * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include "fp4_gemm.h"
-#include "tensorrt_llm/common/customAllReduceUtils.h"
-#include "tensorrt_llm/common/dataType.h"
-#include "tensorrt_llm/common/opUtils.h"
-#include "tensorrt_llm/kernels/communicationKernels/allReduceFusionKernels.h"
-#include "tensorrt_llm/kernels/communicationKernels/moeAllReduceFusionKernels.h"
-#include "tensorrt_llm/kernels/customAllReduceKernels.h"
-#include "tensorrt_llm/kernels/quantization.h"
-#include "tensorrt_llm/runtime/torchUtils.h"
-#include "tensorrt_llm/runtime/utils/mpiUtils.h"
-#include "tensorrt_llm/thop/thUtils.h"
-
-#if ENABLE_MULTI_DEVICE
-#include <ATen/cuda/EmptyTensor.h>
-
-#include <nccl.h>
-#endif // ENABLE_MULTI_DEVICE
-#include <nvml.h>
-#include <torch/extension.h>
-
-#include <cstddef>
-#include <cstdint>
-#include <vector>
-
-namespace torch_ext
-{
-
-#if ENABLE_MULTI_DEVICE
-
-using tensorrt_llm::kernels::AllReduceFusionOp;
-
-namespace
-{
-
-class DeepseekAllreduceOp
-{
-public:
-    DeepseekAllreduceOp() {}
-
-    ~DeepseekAllreduceOp() = default;
-
-    std::vector<torch::Tensor> run(torch::Tensor input, torch::optional<torch::Tensor> workspace,
-        torch::TensorList reduce_fusion_inputs, int64_t rank, int64_t nranks, double eps, int64_t fusion_op) noexcept
-    {
-        auto const fusion_op_type = static_cast<AllReduceFusionOp>(int8_t(fusion_op));
-
-        torch::Tensor residual_out;
-        torch::Tensor norm_out;
-        torch::Tensor quant_out;
-        torch::Tensor scale_out;
-
-        tensorrt_llm::kernels::ar_fusion::AllReduceFusionParams allreduce_fusion_params;
-
-        allreduce_fusion_params.quant_out = nullptr;
-        allreduce_fusion_params.scale_out = nullptr;
-        allreduce_fusion_params.residual_out = nullptr;
-        allreduce_fusion_params.norm_out = nullptr;
-
-        if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_QUANT_NVFP4
-            || fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4)
-        {
-            TORCH_CHECK(reduce_fusion_inputs.size() == 3, "Pre-MLP fusion should have 3 inputs.");
-
-            int64_t sfVecSize = 16;
-            int64_t m = 1;
-            auto const& inputShape = input.sizes();
-            auto const& r = inputShape.size();
-            TORCH_CHECK(r >= 2, "Input should be >=2D tensor.");
-            for (size_t i = 0; i < r - 1; i++)
-            {
-                m *= inputShape[i];
-            }
-            auto const k = inputShape[r - 1];
-            TORCH_CHECK(k % sfVecSize == 0, "Input should be divisible by sfVecSize.");
-            std::vector<int64_t> outputShape(inputShape.begin(), inputShape.end());
-            outputShape[r - 1] = k / 2;
-
-            quant_out = at::detail::empty_cuda(outputShape, FLOAT4_E2M1X2, input.device(), std::nullopt);
-            scale_out = at::detail::empty_cuda({tensorrt_llm::computeFP4SwizzledLayoutSFSize(m, k / sfVecSize)},
-                SF_DTYPE, input.device(), std::nullopt);
-            residual_out = torch::empty_like(reduce_fusion_inputs[0]);
-
-            allreduce_fusion_params.quant_out = quant_out.mutable_data_ptr();
-            allreduce_fusion_params.scale_out = scale_out.mutable_data_ptr();
-            allreduce_fusion_params.residual_out = residual_out.mutable_data_ptr();
-
-            if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4)
-            {
-                norm_out = torch::empty_like(input);
-                allreduce_fusion_params.norm_out = norm_out.mutable_data_ptr();
-                allreduce_fusion_params.pattern
-                    = tensorrt_llm::kernels::ar_fusion::AllReduceFusionPattern::kARResidualRMSNormOutFP4Quant;
-            }
-            else
-            {
-                allreduce_fusion_params.pattern
-                    = tensorrt_llm::kernels::ar_fusion::AllReduceFusionPattern::kARResidualRMSNormFP4Quant;
-            }
-        }
-        else if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM)
-        {
-            norm_out = torch::empty_like(input);
-            residual_out = torch::empty_like(reduce_fusion_inputs[0]);
-
-            allreduce_fusion_params.norm_out = norm_out.mutable_data_ptr();
-            allreduce_fusion_params.residual_out = residual_out.mutable_data_ptr();
-            allreduce_fusion_params.pattern
-                = tensorrt_llm::kernels::ar_fusion::AllReduceFusionPattern::kARResidualRMSNorm;
-        }
-        else
-        {
-            return std::vector<torch::Tensor>();
-        }
-
-        allreduce_fusion_params.nranks = static_cast<int>(nranks);
-        allreduce_fusion_params.rank = static_cast<int>(rank);
-        allreduce_fusion_params.dtype = tensorrt_llm::runtime::TorchUtils::dataType(input.scalar_type());
-        allreduce_fusion_params.size = static_cast<int>(input.numel());
-        allreduce_fusion_params.hidden_dim = static_cast<int>(input.size(-1));
-        allreduce_fusion_params.workspace = reinterpret_cast<void**>(workspace.value().mutable_data_ptr());
-        allreduce_fusion_params.allreduce_in = input.data_ptr();
-        allreduce_fusion_params.residual_in = reduce_fusion_inputs[0].data_ptr();
-        allreduce_fusion_params.rms_gamma = reduce_fusion_inputs[1].data_ptr();
-        allreduce_fusion_params.rms_eps = static_cast<float>(eps);
-        allreduce_fusion_params.use_oneshot = input.size(0) <= tensorrt_llm::kernels::ar_fusion::kOneShotMaxToken;
-
-        if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_QUANT_NVFP4
-            || fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4)
-        {
-            allreduce_fusion_params.scale_factor = static_cast<float*>(reduce_fusion_inputs[2].data_ptr());
-        }
-        else
-        {
-            allreduce_fusion_params.scale_factor = nullptr;
-        }
-
-        allreduce_fusion_params.stream = at::cuda::getCurrentCUDAStream(input.get_device());
-
-        tensorrt_llm::kernels::ar_fusion::allreduce_fusion_op(allreduce_fusion_params);
-
-        if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_QUANT_NVFP4)
-        {
-            return std::vector<torch::Tensor>({quant_out, scale_out, residual_out});
-        }
-        else if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM)
-        {
-            return std::vector<torch::Tensor>({norm_out, residual_out});
-        }
-        else if (fusion_op_type == AllReduceFusionOp::RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4)
-        {
-            return std::vector<torch::Tensor>({norm_out, quant_out, scale_out, residual_out});
-        }
-        else
-        {
-            return std::vector<torch::Tensor>();
-        }
-    }
-
-    std::vector<torch::Tensor> run_moe_allreduce(torch::optional<torch::Tensor> workspace,
-        torch::TensorList reduce_fusion_inputs, int64_t rank, int64_t nranks, double eps, int64_t fusion_op) noexcept
-    {
-
-        auto allreduce_fusion_params = tensorrt_llm::kernels::ar_fusion::moe::MoeReductionAllReduceFusionParams();
-
-        allreduce_fusion_params.quant_out = nullptr;
-        allreduce_fusion_params.scale_out = nullptr;
-        allreduce_fusion_params.residual_out = nullptr;
-        allreduce_fusion_params.norm_out = nullptr;
-
-        allreduce_fusion_params.nranks = static_cast<int>(nranks);
-        allreduce_fusion_params.rank = static_cast<int>(rank);
-        allreduce_fusion_params.dtype
-            = tensorrt_llm::runtime::TorchUtils::dataType(reduce_fusion_inputs[5].scalar_type());
-        // size: num_token * hidden_dim
-        allreduce_fusion_params.size = static_cast<int>(reduce_fusion_inputs[5].numel());
-        allreduce_fusion_params.hidden_dim = static_cast<int>(reduce_fusion_inputs[4].size(-1));
-
-        // workspace: AR scratch space
-        allreduce_fusion_params.workspace = reinterpret_cast<void**>(workspace.value().mutable_data_ptr());
-
-        allreduce_fusion_params.rms_gamma = reduce_fusion_inputs[1].data_ptr();
-        allreduce_fusion_params.rms_eps = static_cast<float>(eps);
-        allreduce_fusion_params.stream = at::cuda::getCurrentCUDAStream(reduce_fusion_inputs[1].get_device());
-
-        allreduce_fusion_params.residual_in = reduce_fusion_inputs[0].data_ptr();
-
-        // MOE Reduction specific params
-        // reduce_fusion_inputs[0]: residual
-        // reduce_fusion_inputs[1]: gamma
-        // reduce_fusion_inputs[2]: moe_reduction_device_num_experts
-        // reduce_fusion_inputs[3]: moe_reduction_scale_input [device_num_experts, m]
-        // reduce_fusion_inputs[4]: moe_reduction_active_experts_token_input [device_num_experts, m, 7168]
-        // reduce_fusion_inputs[5]: moe_reduction_token_input [m, 7168]
-        allreduce_fusion_params.allreduce_in = nullptr; // for safety, set nullptr
-        allreduce_fusion_params.moe_reduction_device_num_experts
-            = static_cast<int*>(reduce_fusion_inputs[2].data_ptr());
-        allreduce_fusion_params.moe_reduction_scale_input = static_cast<float*>(reduce_fusion_inputs[3].data_ptr());
-        allreduce_fusion_params.moe_reduction_active_experts_token_input = reduce_fusion_inputs[4].data_ptr();
-        allreduce_fusion_params.moe_reduction_token_input = reduce_fusion_inputs[5].data_ptr();
-
-        // output tensors
-        torch::Tensor norm_out = torch::empty_like(reduce_fusion_inputs[5]);
-        torch::Tensor residual_out = torch::empty_like(reduce_fusion_inputs[0]);
-
-        allreduce_fusion_params.norm_out = norm_out.mutable_data_ptr();
-        allreduce_fusion_params.residual_out = residual_out.mutable_data_ptr();
-
-        tensorrt_llm::kernels::ar_fusion::moe::moereduction_allreduce_fusion_op(allreduce_fusion_params);
-
-        return std::vector<torch::Tensor>({norm_out, residual_out});
-    }
-};
-
-} // namespace
-
-#endif // ENABLE_MULTI_DEVICE
-
-std::vector<torch::Tensor> deepseekAllreduceFusion(torch::Tensor input, torch::optional<torch::Tensor> workspace,
-    torch::TensorList reduce_fusion_inputs, int64_t const rank, int64_t const nranks, double const eps,
-    int64_t const fusion_op)
-{
-#if ENABLE_MULTI_DEVICE
-    DeepseekAllreduceOp op;
-    auto fusion_op_type = static_cast<AllReduceFusionOp>(int8_t(fusion_op));
-    if (fusion_op_type == AllReduceFusionOp::MOE_ALLREDUCE_RESIDUAL_RMS_NORM)
-    {
-        return op.run_moe_allreduce(workspace, reduce_fusion_inputs, rank, nranks, eps, fusion_op);
-    }
-    else
-    {
-        return op.run(input, workspace, reduce_fusion_inputs, rank, nranks, eps, fusion_op);
-    }
-#else
-    return std::vector<torch::Tensor>();
-#endif // ENABLE_MULTI_DEVICE
-}
-
-} // namespace torch_ext
-
-TORCH_LIBRARY_FRAGMENT(trtllm, m)
-{
-    // reduce_fusion_inputs includes
-    // 0: residual
-    // 1. gamma
-    // 2. scale_factor: only when fusion_op == RESIDUAL_RMS_NORM_QUANT_NVFP4
-    m.def(
-        "deepseek_allreduce_fusion(Tensor input, Tensor? workspace, Tensor[] reduce_fusion_inputs, "
-        "int rank, int nranks, float eps, int fusion_op) -> Tensor[]");
-}
-
-TORCH_LIBRARY_IMPL(trtllm, CUDA, m)
-{
-    m.impl("deepseek_allreduce_fusion", &torch_ext::deepseekAllreduceFusion);
-}

tensorrt_llm/_torch/custom_ops/cpp_custom_ops.py
@@ -192,43 +192,6 @@ def _register_fake():
     def _(input: torch.Tensor, scale: torch.Tensor):
         return torch.empty_like(input).to(torch.float8_e4m3fn), scale
 
-    @torch.library.register_fake("trtllm::deepseek_allreduce_fusion")
-    def _(
-        input,
-        workspace,
-        reduce_fusion_inputs,
-        rank,
-        nranks,
-        eps,
-        fusion_op,
-    ):
-        from tensorrt_llm.functional import AllReduceFusionOp
-        residual = reduce_fusion_inputs[0]
-        if fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4 or fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4:
-            sf_vec_size = 16
-            quant_shape, scale_shape = fp4_utils.get_fp4_shape(
-                input.shape, sf_vec_size)
-            if fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4:
-                return [
-                    input.new_empty(quant_shape, dtype=torch.uint8),
-                    input.new_empty(scale_shape, dtype=torch.uint8),
-                    torch.empty_like(residual)
-                ]
-            else:
-                return [
-                    torch.empty_like(input),
-                    input.new_empty(quant_shape, dtype=torch.uint8),
-                    input.new_empty(scale_shape, dtype=torch.uint8),
-                    torch.empty_like(residual)
-                ]
-
-        elif fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM:
-            return [torch.empty_like(input), torch.empty_like(residual)]
-        elif fusion_op == AllReduceFusionOp.MOE_ALLREDUCE_RESIDUAL_RMS_NORM:
-            return [torch.empty_like(residual), torch.empty_like(residual)]
-        else:
-            raise ValueError(f"Unsupported fusion op: {fusion_op}")
-
     @torch.library.register_fake("trtllm::logits_bitmask")
     def _(logits: List[torch.Tensor], bitmask: List[torch.Tensor]):
         pass

tensorrt_llm/_torch/distributed/ops.py
@@ -1,11 +1,10 @@
 import threading
-from typing import List, Optional, Tuple, Union
+from typing import Optional, Tuple, Union
 
 import torch
 from torch import nn
 
-from tensorrt_llm.functional import (AllReduceFusionOp, AllReduceParams,
-                                     AllReduceStrategy)
+from tensorrt_llm.functional import AllReduceParams, AllReduceStrategy
 from tensorrt_llm.mapping import Mapping
 from tensorrt_llm.plugin.plugin import CustomAllReduceHelper
 
@@ -261,47 +260,3 @@ class MoEAllReduce(nn.Module):
             nranks=self.mapping.tp_size,
             eps=eps,
         )
-
-
-class DeepseekAllReduce(nn.Module):
-
-    def __init__(self, mapping: Mapping):
-        super().__init__()
-        self.mapping = mapping
-        self.workspace = None
-        if self.mapping.tp_size > 1:
-            self.workspace = get_allreduce_workspace(mapping)
-
-    def forward(
-        self,
-        hidden_states: torch.Tensor,
-        reduce_fusion_inputs: List[torch.Tensor],
-        eps: float,
-        fusion_op: AllReduceFusionOp,
-    ) -> Tuple[torch.Tensor, ...]:
-        """
-        hidden_states: hidden_states of the model
-        reduce_fusion_inputs: [residual, norm_weight, scale (if using FP4 quantization)]
-        eps: epsilon for RMSNorm
-        fusion_op: AllReduceFusionOp Type, currently supports RMSNorm:
-          * RESIDUAL_RMS_NORM: allreduce + residual + Norm
-          * RESIDUAL_RMS_NORM_QUANT_NVFP4: allreduce + residual + Norm + fp4 quantization
-        output:
-          * [hidden_states, residual] if using RESIDUAL_RMS_NORM fusion_op
-          * [act_fp4, act_sf, residual] if using RESIDUAL_RMS_NORM_QUANT_NVFP4 fusion_op
-        """
-
-        output = torch.ops.trtllm.deepseek_allreduce_fusion(
-            input=hidden_states,
-            workspace=self.workspace,
-            reduce_fusion_inputs=reduce_fusion_inputs,
-            rank=self.mapping.tp_rank,
-            nranks=self.mapping.tp_size,
-            eps=eps,
-            fusion_op=fusion_op,
-        )
-
-        if len(output) == 0:
-            raise ValueError(f"Unsupported fusion op: {fusion_op}")
-
-        return output

tensorrt_llm/_torch/models/modeling_llama.py
@@ -10,7 +10,7 @@ from transformers.modeling_utils import load_sharded_checkpoint
 from transformers.models.llama4.modeling_llama4 import Llama4MultiModalProjector
 
 from tensorrt_llm._torch.distributed import (AllReduce, AllReduceFusionOp,
-                                             AllReduceParams, DeepseekAllReduce)
+                                             AllReduceParams, MoEAllReduce)
 from tensorrt_llm._torch.pipeline_interface import PipelineInterface
 from tensorrt_llm.functional import PositionEmbeddingType
 from tensorrt_llm.models.convert_utils import split_matrix_tp
@@ -411,7 +411,7 @@ class Llama4DecoderLayer(DecoderLayer):
         self.all_reduce = AllReduce(self.mapping)
         self.next_layer_layernorm: RMSNorm = None
 
-        self.deepseek_allreduce = DeepseekAllReduce(self.mapping)
+        self.moe_allreduce = MoEAllReduce(self.mapping)
 
     def forward(
         self,
@@ -485,17 +485,15 @@ class Llama4DecoderLayer(DecoderLayer):
                 hidden_states_activated_experts = hidden_states[1]
                 num_activated_experts_per_node = hidden_states[2]
                 experts_to_token_score = hidden_states[3]
-                activated_expert_global_ids = hidden_states[4]
-                hidden_states, residual = self.deepseek_allreduce(
-                    hidden_states_activated_experts,  # not used
-                    [
-                        residual, self.next_layer_layernorm.weight,
-                        num_activated_experts_per_node, experts_to_token_score,
-                        hidden_states_activated_experts, shared_output,
-                        activated_expert_global_ids
-                    ],
-                    self.next_layer_layernorm.variance_epsilon,
-                    AllReduceFusionOp.MOE_ALLREDUCE_RESIDUAL_RMS_NORM,
+
+                hidden_states, residual = self.moe_allreduce(
+                    residual,
+                    self.next_layer_layernorm.weight,
+                    device_num_experts=num_activated_experts_per_node,
+                    scale_input=experts_to_token_score,
+                    active_experts_token_input=hidden_states_activated_experts,
+                    token_input=shared_output,
+                    eps=self.next_layer_layernorm.variance_epsilon,
                 )
             else:
                 hidden_states, residual = self.all_reduce(

tests/unittest/_torch/multi_gpu/test_deepseek_allreduce.py
@@ -1,227 +1,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-import pickle
-import sys
-import traceback
-
-import cloudpickle
-import pytest
-import torch
-from mpi4py import MPI
-from mpi4py.futures import MPIPoolExecutor
-from utils.util import skip_pre_blackwell
-
-import tensorrt_llm
-from tensorrt_llm._torch.distributed import (AllReduce, AllReduceFusionOp,
-                                             DeepseekAllReduce)
-from tensorrt_llm._torch.modules.rms_norm import RMSNorm
-from tensorrt_llm.mapping import Mapping
-
-cloudpickle.register_pickle_by_value(sys.modules[__name__])
-MPI.pickle.__init__(
-    cloudpickle.dumps,
-    cloudpickle.loads,
-    pickle.HIGHEST_PROTOCOL,
-)
-
-
-def rms_norm(x: torch.Tensor, weight: torch.Tensor = None, eps: float = 1e-6):
-    y = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + eps)
-    if weight is not None:
-        y = y * weight
-    return y
-
-
-def run_single_rank(tensor_parallel_size, single_rank_forward_func, input,
-                    residual, hidden_size, dtype, fused_add_norm):
-    rank = tensorrt_llm.mpi_rank()
-    torch.cuda.set_device(rank)
-    try:
-        single_rank_forward_func(input, residual, hidden_size, dtype,
-                                 tensor_parallel_size, rank, fused_add_norm)
-    except Exception:
-        traceback.print_exc()
-        raise
-    return True
-
-
-def run_moe_single_rank(tensor_parallel_size, single_rank_forward_func,
-                        token_input, residual, active_experts_token_input,
-                        scale, l0_weight):
-    rank = tensorrt_llm.mpi_rank()
-    torch.cuda.set_device(rank)
-    try:
-        single_rank_forward_func(token_input, residual,
-                                 active_experts_token_input, scale,
-                                 tensor_parallel_size, rank, l0_weight)
-    except Exception:
-        traceback.print_exc()
-        raise
-    return True
-
-
-@torch.inference_mode()
-def row_linear_residual_norm_fusion_forward(
-        x: torch.Tensor, residual: torch.Tensor, hidden_size: int,
-        dtype: torch.dtype, tensor_parallel_size: int,
-        tensor_parallel_rank: int, fusion_op: AllReduceFusionOp):
-
-    x = x.cuda()
-    residual = residual.cuda()
-    norm_weight = torch.randn((hidden_size, ), dtype=dtype, device="cuda")
-    eps = 1e-5
-
-    norm = RMSNorm(hidden_size=hidden_size, eps=eps, dtype=dtype).cuda()
-
-    allreduce = AllReduce(mapping=Mapping(
-        world_size=tensor_parallel_size,
-        tp_size=tensor_parallel_size,
-        rank=tensor_parallel_rank,
-    ), ).cuda()
-
-    deepseek_allreduce = DeepseekAllReduce(mapping=Mapping(
-        world_size=tensor_parallel_size,
-        tp_size=tensor_parallel_size,
-        rank=tensor_parallel_rank,
-    ), ).cuda()
-
-    scale = torch.tensor(1.0, dtype=torch.float32).cuda()
-
-    # Since all the modules here are provided by TRT-LLM,
-    # so it has to be fullgraph compatible
-    def func(input, residual, enable_fusion):
-        xs = torch.chunk(input, 2, dim=-1)
-        if enable_fusion:
-            inter_x = input / tensor_parallel_size
-            if fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM:
-                hidden_states, residual = deepseek_allreduce(
-                    inter_x,
-                    [residual, norm_weight],
-                    eps,
-                    AllReduceFusionOp.RESIDUAL_RMS_NORM,
-                )
-                output = (hidden_states, residual)
-            elif fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4:
-                act_fp4, act_sf, residual = deepseek_allreduce(
-                    inter_x,
-                    [residual, norm_weight, scale],
-                    eps,
-                    AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4,
-                )
-                output = (act_fp4, act_sf, residual)
-            elif fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4:
-                hidden_states, act_fp4, act_sf, residual = deepseek_allreduce(
-                    inter_x,
-                    [residual, norm_weight, scale],
-                    eps,
-                    AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4,
-                )
-                output = (hidden_states, act_fp4, act_sf, residual)
-        else:
-            hidden_states = x
-            inter_output = hidden_states + residual
-            hidden_states = norm(inter_output)
-            output = (hidden_states, residual)
-            if fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4:
-                act_fp4, act_sf = torch.ops.trtllm.fp4_quantize(
-                    hidden_states, scale, 16, False)
-
-                output = (act_fp4, act_sf, residual)
-            elif fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4:
-                act_fp4, act_sf = torch.ops.trtllm.fp4_quantize(
-                    hidden_states, scale, 16, False)
-
-                output = (hidden_states, act_fp4, act_sf, residual)
-
-        return output
-
-    norm.weight.data.copy_(norm_weight)
-
-    output = func(x.clone(), residual.clone(), enable_fusion=False)
-    fuse_output = func(x.clone(), residual.clone(), enable_fusion=True)
-
-    if fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM:
-        h, r = output
-        fh, fr = fuse_output
-
-        torch.testing.assert_close(
-            h,
-            fh,
-            rtol=0.05,
-            atol=0.15,
-        )
-    elif fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4:
-        act_fp4, act_sf, residual = output
-        f_act_fp4, f_act_sf, f_residual = fuse_output
-        torch.testing.assert_close(
-            act_fp4,
-            f_act_fp4,
-            rtol=0.05,
-            atol=0.15,
-        )
-    elif fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4:
-        hidden_states, act_fp4, act_sf, residual = output
-        f_hidden_states, f_act_fp4, f_act_sf, f_residual = fuse_output
-        torch.testing.assert_close(
-            hidden_states,
-            f_hidden_states,
-        )
-
-    # torch run
-    torch_output = x
-    torch_inter_output = torch_output + residual
-    torch_inter_output = torch_inter_output.to(torch.float32)
-    torch_final_output = rms_norm(torch_inter_output, norm_weight,
-                                  eps).to(dtype)
-
-    if fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM or fusion_op == AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4:
-        torch.testing.assert_close(
-            output[0],
-            torch_final_output,
-            rtol=0.05,
-            atol=0.15,
-        )
-
-
-@skip_pre_blackwell
-@pytest.mark.skipif(torch.cuda.device_count() < 2,
-                    reason="needs 2 GPUs to run this test")
-@pytest.mark.parametrize("seq_len", [2, 32], ids=lambda x: f"seqlen:{x}")
-@pytest.mark.parametrize("hidden_size", [7168], ids=lambda x: f"hidden:{x}")
-@pytest.mark.parametrize("fusion_op", [
-    AllReduceFusionOp.RESIDUAL_RMS_NORM,
-    AllReduceFusionOp.RESIDUAL_RMS_NORM_QUANT_NVFP4,
-    AllReduceFusionOp.RESIDUAL_RMS_NORM_OUT_QUANT_NVFP4,
-],
-                         ids=[
-                             "residual_rms_norm",
-                             "residual_rms_norm_quant_nvfp4",
-                             "residual_rms_norm_out_quant_nvfp4"
-                         ])
-def test_row_linear_residual_norm_fusion(seq_len, hidden_size, fusion_op):
-    torch.manual_seed(42)
-    dtype = torch.bfloat16
-    tensor_parallel_size = 2
-    x = torch.randn((seq_len, hidden_size), dtype=dtype)
-    residual = torch.randn_like(x)
-    with MPIPoolExecutor(max_workers=tensor_parallel_size) as executor:
-        results = executor.map(
-            run_single_rank,
-            *zip(*[(tensor_parallel_size,
-                    row_linear_residual_norm_fusion_forward, x, residual,
-                    hidden_size, dtype, fusion_op)] * 2),
-        )
-        for r in results:
-            assert r is True

commit 1bbf1b6a217b2e40a1afd337ff869064f328fae4

chore: Fix pipeline break caused by previous PR (#4081) rebase + pipeline reuse (#4169)
Fix import break caused by rebase.

Signed-off-by: Yukun He <23156053+hyukn@users.noreply.github.com>

tensorrt_llm/_torch/distributed/__init__.py
@@ -1,7 +1,8 @@
+from tensorrt_llm.functional import AllReduceFusionOp
+
 from .communicator import Distributed, MPIDist, PPComm, TorchDist
-from .ops import (AllReduce, AllReduceFusionOp, AllReduceParams,
-                  AllReduceStrategy, MoEAllReduce, allgather, reducescatter,
-                  userbuffers_allreduce_finalize)
+from .ops import (AllReduce, AllReduceParams, AllReduceStrategy, MoEAllReduce,
+                  allgather, reducescatter, userbuffers_allreduce_finalize)
 
 __all__ = [
     "allgather",